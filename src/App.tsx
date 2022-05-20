23e71a05c0160002ffbc926b225ca29922cef619
Jake <jake@jake.nz>
2022-05-20 15:20:38 +1000

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import { Items } from "./features/items/Items";
 import "./App.css";
 
 function App() {
-  const [x, setX] = useState(1);
-
   console.log("app render");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Items />
-        <button onClick={() => setX((x) => x + 1)}>Rerender App {x}</button>
       </header>
     </div>
   );
--- src/features/items/itemsSlice.ts
+++ src/features/items/itemsSlice.ts
@@ -15,9 +15,7 @@ const initialState: ItemsState = {
 export const itemsSlice = createSlice({
   name: "items",
   initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    // Use the PayloadAction type to declare the contents of `action.payload`
     replace: (state, action: PayloadAction<ItemsState>) => action.payload,
   },
 });
